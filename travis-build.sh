03ea1942a8333b20d1d30a9d678a5b6916250e92
Serban Iordache <iordache@hotmail.com>
2016-09-20 22:03:38 +0200

    build groovydoc

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-./gradlew --no-daemon --info --stacktrace build asciidoc
+./gradlew --no-daemon --info --stacktrace build asciidoc groovydoc
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   if [ "`git ls-remote origin gh-pages`" == "" ]; then
